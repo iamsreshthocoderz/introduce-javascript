217f0d31ae8763159c2ef010019a31c9aab211f1
iamsreshthocoderz <sreshthot@gmail.com>
2023-07-31 02:22:48 +0600

    first commit

---
+++ java.js
@@ -0,0 +1,91 @@
+// // var fruits = ['apple', 'banna', 'orange'];
+// // console.log(fruits);
+// // var index = fruits.indexOf('banna');
+
+// // console.log(index);
+
+// // fruits[1]='mango'
+
+// // fruits.pop();
+// // fruits.push('watermelon')
+// // fruits.unshift('mango')
+// // fruits.indexOf('banna')
+// // delete fruits[1]
+// // console.log(fruits);
+
+
+// // var me = 85
+// // , tom = 66 
+// // , jane =95
+// // , peter =56
+// //  ,john = 40;
+// // var number = peter;
+// // if (number >= 80){
+// //     console.log('grade = A')
+// // }
+
+// // else if(number >=60){
+// //     console.log('grade = B')
+// // }
+
+// // else if(number >=50){
+// //     console.log('grade = C')
+// // }
+// // else if (number >=40){
+// //     console.log('grade = D')
+// // }
+
+// // else if (number <=39) {
+// //     console.log('grade = F')
+// // }
+
+
+// var number = [13, 79, 45];
+// var max;
+
+// if (number[0]>number[1]){
+//     max = number[0];
+    
+// }
+
+
+// else{
+//     max=number[1];
+// }
+
+// if (max>number[2]){
+//     max = max;
+// }
+
+// else {
+//     max=number[2];
+// }
+
+// console.log(max);
+
+
+// var side1 = 6
+// var side2 = 6
+// var side3 = 9
+
+// if (side1==side2){
+//     console.log('Isosceles');
+// }
+
+// else if(side1==side3){
+//     console.log('Isosceles');
+// }
+
+// else if(side2==side3){
+//     console.log('Isosceles');
+// }
+// else{
+//     console.log('notIsosceles');
+// }
+
+
+var number = 1;
+while(number<= 100){
+    console.log(number);
+    number++;
+}
